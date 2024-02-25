a2e177df7646b1ba7b76756c9412402aec008dfd
vinodSupnekar <vinod.supnekar@dazn.com>
2024-02-25 06:49:16 +0530

    Initial Commit

---
+++ RxSwiftApp/RxSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RxSwiftAppApp.swift
+//  RxSwiftApp
+//
+//  Created by Vinod Supnekar on 25/02/24.
+//
+
+import SwiftUI
+
+@main
+struct RxSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
